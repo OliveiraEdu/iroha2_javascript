709449e228f82acb097463df3ac69bc709ad4554
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2024-01-15 13:46:22 -0300

    Update Signer.ts

--- Signer.ts
+++ Signer.ts
@@ -1,6 +1,16 @@
 import { cryptoTypes } from '@iroha2/crypto-core'
 import { Signer } from '@iroha2/client'
 import { datamodel } from '@iroha2/data-model'
+import { crypto } from '@iroha2/crypto-target-node'
+
+const keyPair = crypto.KeyPair.fromJSON({
+  public_key: 'ed0120e555d194e8822da35ac541ce9eec8b45058f4d294d9426ef97ba92698766f7d3',
+  private_key: {
+    digest_function: 'ed25519',
+    payload:
+      'de757bcb79f4c63e8fa0795edc26f86dfdba189b846e903d0b732bb644607720e555d194e8822da35ac541ce9eec8b45058f4d294d9426ef97ba92698766f7d3',
+  },
+})
 
 // Key pair from previous step
 declare const keyPair: cryptoTypes.KeyPair
