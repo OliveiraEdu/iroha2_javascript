788512e79521d969e6311980ddb44e492c43f7b8
eduardo <edurdocostaoliveira@gmail.com>
2024-01-16 19:26:25 -0300

    example.md

--- example.ts
+++ example.ts
@@ -35,18 +35,16 @@ const accountId = AccountId({
 const signer = new Signer(accountId, keyPair)
 
 
+// Define Torii requirements
 const toriiRequirements: ToriiRequirementsForApiHttp &
   ToriiRequirementsForApiWebSocket &
   ToriiRequirementsForTelemetry = {
   apiURL: 'http://127.0.0.1:8080',
   telemetryURL: 'http://127.0.0.1:8081',
   ws: WS,
-  fetch:
-    // passing globally available `fetch`, but binding it to `window`
-    // to avoid `TypeError: "'fetch' called on an
-    //           object that does not implement interface Window."`
-    fetch.bind(window),
+  fetch: typeof window !== 'undefined' ? fetch.bind(window) : fetch,
 }
+
 
 // --snip--
 declare const signer: Signer
